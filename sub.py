17eb557f88cd4a407fc3094886edbedae34e16b0
virat <virat.meduri@gmail.com>
2022-02-22 21:13:32 +0530

    raise PR

---
+++ sub.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sub = num1 - num2
+
+# Display the sub
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
